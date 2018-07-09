fe6a3b9bf27f19cb46cc4b12d56356245b3e87af
Liang Yuan <superylbyr@gmail.com>
2018-07-08 23:42:04 -0700

    Add controller

--- src/main/java/io/controller/AdsController.java
+++ src/main/java/io/controller/AdsController.java
@@ -1,21 +1,32 @@
 package io.controller;
 
-//
-//import java.util.concurrent.atomic.AtomicLong;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class GreetingController {
-//
-//    private static final String template = "Hello, %s!";
-//    private final AtomicLong counter = new AtomicLong();
-//
-//    @RequestMapping("/greeting")
-//    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-//        return new Greeting(counter.incrementAndGet(),
-//                            String.format(template, name));
-//    }
-//}
+
+import io.data.Ad;
+import io.service.AdsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+public class AdsController {
+
+    private AdsService adsService;
+
+    @Autowired
+    public AdsController(AdsService adsService) {
+        this.adsService = adsService;
+    }
+
+    @RequestMapping("/ad")
+    public List<Ad> getAds(@RequestParam(value = "query") String query, @RequestParam(value = "id") String deviceId,
+                           @RequestHeader(value = "X-Forwarded-For", required = false, defaultValue = "1010") String deviceIp, @RequestParam(value = "class") String queryClass,
+                           @RequestParam(value = "count", required = false, defaultValue = "5") int count) throws IOException {
+        return this.adsService.selectAds(query, deviceId, deviceIp, queryClass, count);
+    }
+}
 
